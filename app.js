c492727699ebff56c25f5631325703944af93046
kehdcode <kehindedawood@gmail.com>
2018-12-21 03:36:57 +0100

    created the landing page and camp ground template

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ var express = require("express");
 
 var app = express();
 
+//This helps to avoid writting our file extention in the routes
+app.set("view engine", "ejs");
+
+//Routes
 app.get("/", function(req, res){
-    res.send("This is our landing page");
+    res.render("landing");
 });
 
-app.listen(3000 , function(){
+app.get("/campgrounds", function(req,res){
+    
+    var campGround = [
+        {name: "Yankari Reserve", images: "https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg"},
+        {name: "Lekke Reserve", images: "https://www.photosforclass.com/download/flickr-2602356334"},
+        {name: "Salmon Reserve", images: "https://www.photosforclass.com/download/flickr-2984119099"}
+    ]
+    res.render("campgrounds", {camps: campGround});
+})
+
+
+app.listen(4001 , function(){
     console.log("Camp share sever has started");
 })
